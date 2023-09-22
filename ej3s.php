7174d0b8fc8f6fc37bde87793febd21f9cfa8dcf
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-09-22 09:30:16 +0200

    ej3s.php a medias

--- ej3s.php
+++ ej3s.php
@@ -1,6 +1,29 @@
 <html>
 <head><title>IP + NETMASK + etc. etc.</title></head>
 <body>
+    <?php
+    $ip = '192.168.16.100/16';
+
+    $netMask = getNetmask($ip);
     
+
+    // ----------------------- PRINTS ----------------------
+    print("<p>IP $ip</p>");
+    print("<p>Máscara $netMask </p>");
+    
+    
+    
+    
+    
+    
+    
+    
+    // ----------------------- FUNCIONES --------------------
+    function getNetmask($ip){
+        $netmaskPos = strpos($ip,"/");
+        $netmask = substr($ip, $netmaskPos+1);
+        return $netmask;
+    }
+    ?>
 </body>
 </html>
