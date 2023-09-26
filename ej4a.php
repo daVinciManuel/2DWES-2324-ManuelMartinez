6db5bb73690efb3fab9014d91a009370eb1e4202
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-09-26 10:31:35 +0200

    Added: ej4 Arrays

---
+++ ej4a.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>3. Arrays</title>
+</head>
+<body>
+   <table border="1">
+   <tr>
+    <th>Indice</th>
+    <th>Binario</th>
+    <th>Octal</th>
+   </tr> 
+   <tr>
+    <?php
+    for($i = 0; $i < 20; $i++){
+        $bin[19-$i] = decbin($i);
+        $oct[$i] = decoct($i);
+
+    }
+    for($i = 0; $i < 20; $i++){
+        print("<tr><td>$i</td><td>$bin[$i]</td><td>$oct[$i]</td></tr>");
+    }
+    ?>
+   </tr>
+   </table> 
+</body>
+</html>
