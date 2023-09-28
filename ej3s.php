d22c73c020aea9d9ef56c120c2815641d8005973
daVinciManuel <mgmt.manuel02@gmail.com>
2023-09-29 01:25:06 +0200

    Revert "ej3s.php a medias"
    This reverts commit 7174d0b8fc8f6fc37bde87793febd21f9cfa8dcf.

--- ej3s.php
+++ ej3s.php
@@ -5,12 +5,13 @@
     $ip = '192.168.16.100/16';
 
     $netMask = getNetmask($ip);
-    $DireccionRed = getDireccionRed($ip);
+    
 
     // ----------------------- PRINTS ----------------------
     print("<p>IP $ip</p>");
     print("<p>Máscara $netMask </p>");
-    print("<p>Direccion Red $DireccionRed </p>");    
+    
+    
     
     
     
@@ -23,36 +24,6 @@
         $netmask = substr($ip, $netmaskPos+1);
         return $netmask;
     }
-    function getDireccionRed($ip){
-        $netmask = getNetmask($ip);
-        $ipArray = explode(".",$ip);
-        
-    }
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // ~~~~~~~~~~~~~~~~~~~ INTENTO FALLIDO ~~~~~~~~~~~~~~~~~~~ 
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // [pensaba que todas las mascaras eran multiplos de 8]
-    //
-    // function getDireccionRed($ip){
-    //     // ============== DATOS ====================
-    //     // ________________________________________
-    //     // obtengo la posicion de '/' en la IP
-    //     $netmaskPos = strpos($ip,"/");
-    //     // obtengo la máscara
-    //     $netmask = substr($ip, $netmaskPos+1);
-    //     $ipArrayLength = count($ipArray);
-    //     // ============== OPERACIONES ====================
-    //     // _______________________________________________
-    //     $aux = $netmask / 8;
-    //     for($i = 0; $i < $ipArrayLength; $i++){
-    //         if($i >= $aux){
-    //             $ipArray[$i] = 0;
-    //         }
-    //     }
-    //     $newIp = "$ipArray[0].$ipArray[1].$ipArray[2].$ipArray[3]";
-    //     return $newIp;
-    // }
-    // // echo getDireccionRed($ip);
     ?>
 </body>
 </html>
