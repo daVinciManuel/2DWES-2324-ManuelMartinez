565658268ba83c112a039b7cba510db9a1fe8505
daVinciManuel <mgmt.manuel02@gmail.com>
2023-09-29 01:22:05 +0200

    ej3s fail

--- ej3s.php
+++ ej3s.php
@@ -23,31 +23,36 @@
         $netmask = substr($ip, $netmaskPos+1);
         return $netmask;
     }
-    
     function getDireccionRed($ip){
-        // ============== DATOS ====================
-        // ________________________________________
-        // obtengo la posicion de '/' en la IP
-        $netmaskPos = strpos($ip,"/");
-        // obtengo la máscara
-        $netmask = substr($ip, $netmaskPos+1);
-        // separo la IP de la máscara
-        $ipOnly = substr($ip,0,$netmaskPos);
-        // array con los componentes del IPv4
-        $ipArray = explode(".",$ipOnly);
-        $ipArrayLength = count($ipArray);
-        // ============== OPERACIONES ====================
-        // _______________________________________________
-        $aux = $netmask / 8;
-        for($i = 0; $i < $ipArrayLength; $i++){
-            if($i >= $aux){
-                $ipArray[$i] = 0;
-            }
-        }
-        $newIp = "$ipArray[0].$ipArray[1].$ipArray[2].$ipArray[3]";
-        return $newIp;
+        $netmask = getNetmask($ip);
+        $ipArray = explode(".",$ip);
+        
     }
-    // echo getDireccionRed($ip);
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~ INTENTO FALLIDO ~~~~~~~~~~~~~~~~~~~ 
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // [pensaba que todas las mascaras eran multiplos de 8]
+    //
+    // function getDireccionRed($ip){
+    //     // ============== DATOS ====================
+    //     // ________________________________________
+    //     // obtengo la posicion de '/' en la IP
+    //     $netmaskPos = strpos($ip,"/");
+    //     // obtengo la máscara
+    //     $netmask = substr($ip, $netmaskPos+1);
+    //     $ipArrayLength = count($ipArray);
+    //     // ============== OPERACIONES ====================
+    //     // _______________________________________________
+    //     $aux = $netmask / 8;
+    //     for($i = 0; $i < $ipArrayLength; $i++){
+    //         if($i >= $aux){
+    //             $ipArray[$i] = 0;
+    //         }
+    //     }
+    //     $newIp = "$ipArray[0].$ipArray[1].$ipArray[2].$ipArray[3]";
+    //     return $newIp;
+    // }
+    // // echo getDireccionRed($ip);
     ?>
 </body>
 </html>
